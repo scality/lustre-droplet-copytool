216a4dc107d930f308d36b066ef3ff1abefd3574
conan_o <olivier.conan@epitech.eu>
2015-09-15 10:34:56 +0200

    Added few fixes/retrieve-options on opt_get

--- include/cpt_opt.h
+++ include/cpt_opt.h
@@ -1,12 +1,18 @@
 #ifndef		CPT_OPT_H
 # define	CPT_OPT_H
 
-# include	<stdbool.h>
+# include	<asm/types.h>
+
+# define	MAX_ARCH	(sizeof(__u32) * 8)
 
 typedef struct	s_opt
 {
   int		is_daemon;
   int		is_verbose;
+  int		arch_ind[MAX_ARCH];
+  unsigned int	arch_ind_count;
+  char		*ring_mp;
+  char		*lustre_mp;
 }		t_opt;
 
 #endif		/* CPT_OPT_H_ */
--- srcs/main.c
+++ srcs/main.c
@@ -14,7 +14,15 @@
 void		init_opt(t_opt *cpt_opt)
 {
   cpt_opt->is_daemon = 0;
+  cpt_opt->is_verbose = 0;
+  cpt_opt->arch_ind_count = 0;
+  cpt_opt->ring_mp = NULL;
+  cpt_opt->lustre_mp = NULL;
 }
+
+/*
+** Opt_get.
+*/
 
 int		get_opt(int ac, char **av, t_opt *cpt_opt)
 {
@@ -22,27 +30,45 @@ int		get_opt(int ac, char **av, t_opt *cpt_opt)
   struct option	long_opts[] = {
     {"archive",	required_argument,	NULL,			'A'},
     {"daemon",	no_argument,		&cpt_opt->is_daemon,	1},
+    {"ring",	required_argument,	0,			'r'},
+    {"help",	no_argument,		0,			'h'},
     {"verbose",	no_argument,		0,			'v'},
     {0, 0, 0, 0}
   };
   int		opt_ind = 0;
 
-  if (ac == 1)
-    return(usage());
-  while ((trig = getopt_long(ac, av, "A:dv", long_opts, &opt_ind)) != -1)
+  optind = 0;
+  while ((trig = getopt_long(ac, av, "A:r:hv", long_opts, &opt_ind)) != -1)
     switch (trig)
       {
       case 'A':
-	fprintf(stdout, "(dev msg) archive.\n");
+	if ((cpt_opt->arch_ind_count >= MAX_ARCH)
+	    || ((unsigned int)atoi(optarg) >= MAX_ARCH))
+	  {
+	    fprintf(stdout, "Archive number must be less"
+		    "than %zu.\n", MAX_ARCH);
+	    return (-E2BIG);
+	  }
+	cpt_opt->arch_ind[cpt_opt->arch_ind_count++] = atoi(optarg);
+	if (cpt_opt->arch_ind_count <= MAX_ARCH)
+	  cpt_opt->arch_ind[cpt_opt->arch_ind_count] = 0;
 	break;
-      case 'd':
-	cpt_opt->is_daemon = 1;
-        break;
+      case 'r':
+	cpt_opt->ring_mp = optarg;
+	break;
+      case 'h':
+	usage();
+	return (-EINVAL);
       case 'v':
-	fprintf(stdout, "(dev msg) verbosing.\n");
+	cpt_opt->is_verbose = 1;
 	break;
       }
-  fprintf(stdout, "(dev msg) dameon = %d\n", cpt_opt->is_daemon);
+  if (ac != optind + 1)
+    {
+      fprintf(stdout, "No lustre mount point specified.\n");
+      return (-EINVAL);
+    }
+  cpt_opt->lustre_mp = av[optind];
   return (1);
 }
 
@@ -57,14 +83,14 @@ int		daemonize(t_opt *cpt_opt)
   if (cpt_opt->is_daemon)
     {
       if ((ret = daemon(1, 1)) < 0)
-	return ((ret = errno));
+	return (-(ret = errno));
       fprintf(stdout, "Daemon created.\n");
     }
   return (1);
 }
 
 /*
-**
+** Main.
 */
 
 int		main(int ac, char **av)
@@ -73,10 +99,24 @@ int		main(int ac, char **av)
   t_opt		cpt_opt;
 
   init_opt(&cpt_opt);
-  if ((ret = get_opt(ac, av, &cpt_opt)) == EINVAL)
-    return (EINVAL);
+  if ((ret = get_opt(ac, av, &cpt_opt)) < 0)
+    return (ret);
   if (cpt_opt.is_daemon)
-    daemonize(&cpt_opt);
-  printf("Construction checked.\n");
+    if ((ret = (daemonize(&cpt_opt))) < 0)
+      return (ret);
+  fprintf(stdout, "(dev msg) Values retrieved :\n"
+	  "is_daemon : %d\n"
+	  "is_verbose : %d\n"
+	  "ring_mp : %s\n"
+	  "lustre_mp : %s\n",
+	  cpt_opt.is_daemon,
+	  cpt_opt.is_verbose,
+	  cpt_opt.ring_mp,
+	  cpt_opt.lustre_mp);
+  unsigned int	i;
+  for (i = 0; i < cpt_opt.arch_ind_count; i += 1)
+    fprintf(stdout, "(dev msg) Archive index retrieved :\n"
+	    "index %d\n",
+	    cpt_opt.arch_ind[i]);
   return (1);
 }
